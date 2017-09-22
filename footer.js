37c55308f807e47c4b4f914c49c642d52f3f0fe3
744893428@qq.com <minsongtao@wanglibank.com>
2017-09-22 18:53:40 +0800

    泰山攻略新增

---
+++ footer.js
@@ -0,0 +1,19 @@
+document.writeln("<!-- FOOTER -->");
+document.writeln("<footer>");
+document.writeln("<p class=\"pull-right\"><a href=\"#\">置顶</a></p>");
+document.writeln(" <p>&copy; 2017 Deweyblog, Inc. &middot; <a href=\"#\">Privacy</a> &middot; <a href=\"#\">Terms</a></p>");
+document.writeln(" </footer>");
+document.writeln("</div><!-- /.container -->");
+document.writeln("<!-- Bootstrap core JavaScript================================================== -->");
+document.writeln("<!-- Placed at the end of the document so the pages load faster -->");
+document.writeln("<!-- <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js\"></script> -->");
+document.writeln("<script src=\"http://127.0.0.1/EstaDewey.github.io/js/jquery-3.2.1.min.js\"></script>");
+document.writeln("<script src=\"http://127.0.0.1/EstaDewey.github.io/js/vendor/jquery.min.js\"><\/script>");
+document.writeln("<script src=\"http://127.0.0.1/EstaDewey.github.io/js/bootstrap.min.js\"></script>");
+document.writeln("<!-- Just to make our placeholder images work. Don't actually copy the next line! -->");
+document.writeln("<script src=\"http://127.0.0.1/EstaDewey.github.io/js/vendor/holder.min.js\"></script>");
+document.writeln("<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->");
+document.writeln("<script src=\"http://127.0.0.1/EstaDewey.github.io/js/ie10-viewport-bug-workaround.js\"></script>");
+document.writeln("</body>");
+document.writeln("</html>");
+
